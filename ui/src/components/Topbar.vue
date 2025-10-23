4dfed84cb1e64b330a230059c48925f822403eb7
SAZAUNI <santiago.zarza@fiuni.edu.py>
2025-10-23 13:35:22 -0300

    [Fix]: corrección en los íconos svg, y el título del TopBar.vue

--- ui/src/components/Topbar.vue
+++ ui/src/components/Topbar.vue
@@ -16,7 +16,7 @@
           >
             <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20" />
           </svg>
-          <h1 class="app-name">Biblioteca Digital</h1>
+          <h1 class="app-name">DigiBooks Source</h1>
         </div>
       </div>
 
