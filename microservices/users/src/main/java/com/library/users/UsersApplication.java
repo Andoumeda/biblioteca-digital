a0f0e6f060783abd282a00d3f6744cfc71009117
Arturo-AlonoBS <arturoalonb@gmail.com>
2025-10-28 12:22:11 -0300

    Modificaciones en eureka server

--- microservices/users/src/main/java/com/library/users/UsersApplication.java
+++ microservices/users/src/main/java/com/library/users/UsersApplication.java
@@ -3,8 +3,9 @@ package com.library.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 @EntityScan(basePackages = {"com.library.entities"})
 public class UsersApplication {
     public static void main(String[] args) {
