d09150791dbed9e2f8496538c256a82b53c44a24
SAZAUNI <santiago.zarza@fiuni.edu.py>
2025-11-02 17:50:49 -0300

    [REFACTOR]: cambio ne el directorio del módulo de security

--- auth/security/src/main/java/com/library/security/exceptions/TokenExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.library.security.exceptions;
-
-public class TokenExpiredException extends RuntimeException {
-    public TokenExpiredException() {
-        super("Token expirado");
-    }
-}
